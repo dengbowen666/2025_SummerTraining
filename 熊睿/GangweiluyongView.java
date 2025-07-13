21726fcc6c2ea129bf43fc659e8b4c108939caae
熊睿 <3367257165@qq.com>
2025-07-13 14:45:52 +0800

    岗位录用

---
+++ 熊睿/GangweiluyongView.java
@@ -0,0 +1,35 @@
+package com.entity.view;
+
+import com.entity.GangweiluyongEntity;
+
+import com.baomidou.mybatisplus.annotations.TableName;
+import org.apache.commons.beanutils.BeanUtils;
+import java.lang.reflect.InvocationTargetException;
+
+import java.io.Serializable;
+ 
+
+/**
+ * 岗位录用
+ * 后端返回视图实体辅助类
+ * @author 
+ * @email 
+ * @date
+ */
+@TableName("gangweiluyong")
+public class GangweiluyongView  extends GangweiluyongEntity implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public GangweiluyongView(){
+	}
+ 
+ 	public GangweiluyongView(GangweiluyongEntity gangweiluyongEntity){
+ 	try {
+			BeanUtils.copyProperties(this, gangweiluyongEntity);
+		} catch (IllegalAccessException | InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+ 		
+	}
+}
